ba87f6daba1faf58fc700d00c10c6ff983f8f0e5
Alejandro Lynch <alynch@hopscotch.ca>
2025-05-26 22:15:10 -0400

    Modify passing data to Svelte component from Liveview

--- assets/svelte/Simple.svelte
+++ assets/svelte/Simple.svelte
@@ -1,19 +1,18 @@
 <script>
-    /** @type {{number: any, live: any}} */
-    let {game, number, live} = $props()
+    let {game, live} = $props()
 
     function increase() {
-        live.pushEvent("set_number", {number: number + 1})
+        live.pushEvent("set_number", {number: game.puzzle_data.n + 1})
     }
 
     function decrease() {
-        live.pushEvent("set_number", {number: number - 1})
+        live.pushEvent("set_number", {number: game.puzzle_data.n - 1})
     }
 </script>
 
-<h1>{ game }</h1>
+<h1>{ game.game_name }</h1>
 
-<p>The number is {number}</p>
+<p>The number is {game.puzzle_data.n}</p>
 
 <button onclick={increase}>+</button>
 <button onclick={decrease}>-</button>
--- lib/games_web/live/game_live.ex
+++ lib/games_web/live/game_live.ex
@@ -1,24 +1,36 @@
 defmodule GamesWeb.GameLive do
   use GamesWeb, :live_view
+  import Ecto.Query
+  alias Phoenix.Component, as: Live
 
   alias Games.Admin
   alias Games.Admin.Game
+  alias Games.Admin.Puzzle
 
   def render(assigns) do
     ~H"""
-      <LiveSvelte.svelte name="Simple" props={%{game: @game, number: @number}} />
+      <LiveSvelte.svelte name="Simple" props={%{game: @game}} />
     """
   end
 
   def mount(params, _session, socket) do
     id = params["id"]
-    game =   Admin.get_game_by!(id)
+    date = params["date"]
 
-    {:ok, assign(socket, game: game.name, number: 5)}
+    game = from g in Game,
+      left_join: p in Puzzle, on: g.id == p.game_id,
+      where: g.slug == ^id and p.date == ^date,
+      select: %{game_name: g.name, puzzle_name: p.name, puzzle_date: p.date, puzzle_data: p.data}
+
+    {:ok, game} = Games.Repo.one(game)
+    |> Map.get_and_update(:puzzle_data, &Jason.decode/1)
+    # |> IO.inspect()
+
+    {:ok, assign(socket, game: game)}
   end
 
   def handle_event("set_number", %{"number" => number}, socket) do
-    {:noreply, assign(socket, :number, number)}
+    {:noreply, Live.update(socket, :game, fn game -> Map.put(game, :puzzle_data, %{"n" => number}) |> IO.inspect end)}
   end
 
 end
