05670d551a116bb62ea911796f763318188616d0
Alejandro Lynch <alynch@hopscotch.ca>
2025-05-29 14:03:12 -0400

    Add functionality to edit and delete puzzles

--- lib/games_web/controllers/admin/puzzle_controller.ex
+++ lib/games_web/controllers/admin/puzzle_controller.ex
@@ -22,4 +22,35 @@ defmodule GamesWeb.Admin.PuzzleController do
         render(conn, "new.html", changeset: changeset, game: game)
     end
   end
+
+
+  def edit(conn, %{"id" => id}) do
+    puzzle = Admin.get_puzzle!(id)
+    changeset = Admin.change_puzzle(puzzle)
+    render(conn, :edit, puzzle: puzzle, changeset: changeset)
+  end
+
+
+  def update(conn, %{"id" => id, "puzzle" => puzzle_params}) do
+    puzzle = Admin.get_puzzle!(id)
+
+    case Admin.update_puzzle(puzzle, puzzle_params) do
+      {:ok, puzzle} ->
+        conn
+        |> put_flash(:info, "Puzzle updated successfully.")
+        |> redirect(to: ~p"/admin/games/#{puzzle.game_id}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :edit, puzzle: puzzle, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    puzzle = Admin.get_puzzle!(id)
+    {:ok, _puzzle} = Admin.delete_puzzle(puzzle)
+
+    conn
+    |> put_flash(:info, "Puzzle deleted successfully.")
+    |> redirect(to: ~p"/admin/games")
+  end
 end
